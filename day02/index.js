4285867cd633477ac89a48dc68c505dc80bbd0a2
Neil Italia <neilitalia1998@gmail.com>
2021-12-02 14:57:59 -0600

    :sparkles: solve day 2 part 2

--- day02/index.js
+++ day02/index.js
@@ -27,4 +27,32 @@ const part1 = () => {
   return forward * depth
 }
 
+const part2 = () => {
+  let forward = 0
+  let depth = 0
+  let aim = 0
+
+  data.forEach((e)=>{
+    const [direction, distance] = e.split(" ")
+
+    switch (direction) {
+      case "forward":
+        forward += parseInt(distance)
+        depth += aim * parseInt(distance)
+        break;
+      case "down":
+        aim += parseInt(distance)
+        break;
+      case "up":
+        aim -= parseInt(distance)
+        break;
+      default:
+        break;
+    }
+  })
+
+  return forward * depth
+}
+
 console.log('Part 1 Answer: ', part1())
+console.log('Part 2 Answer: ', part2())
